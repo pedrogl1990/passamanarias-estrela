0eff6b8f55289e6a85856643248ddb2be6f24a12
Pedro Leite <pgleite1990@gmail.com>
2022-04-28 22:27:49 +0100

    estilização da página contactos e respetivos formulários

--- src/components/contact/ContactContent.jsx
+++ src/components/contact/ContactContent.jsx
@@ -1,3 +1,4 @@
+import "./ContactContent.css";
 export default function ContactContent() {
   return (
     <>
@@ -11,58 +12,88 @@ export default function ContactContent() {
           </p>
         </section>
         <section className="contact-section-form">
-          <form action="">
-            <imput
-              nome="name"
-              id="name"
-              type="text"
-              placeholder="Nome / Empresa:*"
-              required
-            ></imput>
-            <imput
-              name="email"
-              id="email"
-              type="email"
-              placeholder="Email:*"
-              required
-            ></imput>
-            <imput
-              name="tel"
-              id="tel"
-              type="tel"
-              placeholder="Telefone:*"
-              required
-            ></imput>
-            <imput
-              name="assunto"
-              id="assunto"
-              type="text"
-              placeholder="Assunto:*"
-              required
-            ></imput>
-            <textarea
-              name="message"
-              id="message"
-              cols="30"
-              rows="10"
-              placeholder="Mensagem:*"
-              required
-            ></textarea>
-            <button type="submit">Enviar</button>
+          <form>
+            <div className="contact-input">
+              <label htmlFor="name">
+                <input
+                  style={{ width: 500, height: 40 }}
+                  className="input-decoration"
+                  name="name"
+                  id="name"
+                  type="text"
+                  placeholder="Nome / Empresa:*"
+                />
+              </label>
+            </div>
+            <div className="contact-input">
+              <label htmlFor="email">
+                <input
+                  style={{ width: 500, height: 40 }}
+                  className="input-decoration"
+                  name="email"
+                  id="email"
+                  type="text"
+                  placeholder="Email:*"
+                />
+              </label>
+            </div>
+            <div className="contact-input">
+              <label htmlFor="tel">
+                <input
+                  style={{ width: 500, height: 40 }}
+                  className="input-decoration"
+                  name="tel"
+                  id="tel"
+                  type="text"
+                  placeholder="Telefone:*"
+                />
+              </label>
+            </div>
+            <div className="contact-input">
+              <label htmlFor="assunto">
+                <input
+                  style={{ width: 500, height: 40 }}
+                  className="input-decoration"
+                  name="assunto"
+                  id="assunto"
+                  type="text"
+                  placeholder="Assunto:*"
+                />
+              </label>
+            </div>
+            <div className="contact-input">
+              <textarea
+                style={{ width: 500, height: 250 }}
+                className="input-decoration"
+                name="message"
+                id="message"
+                cols="30"
+                rows="10"
+                placeholder="Mensagem:*"
+                required
+              ></textarea>
+            </div>
+            <button className="contact-button" type="submit">
+              Enviar
+            </button>
           </form>
         </section>
         <section className="contact-map"></section>
-        <div className="square-links-left">
-          <img src="images/Conteudo/fita.png" alt="fita de algodão" />
-          <h2>Produtos</h2>
-          <button>Ver Mais</button>
-        </div>
-        <div className="square-links-right">
-          <img src="images/Conteudo/contacto.png" alt="portátil e telemovel" />
-          <h2>Contactos</h2>
-          <button>Ver Mais</button>
-        </div>
-        <img src="images/Icones/tres-rolos-fita.png" alt="tres rolos de fita" />
+        <section className="contact-bottom-links">
+          <div className="contact-bottom-square-1">
+            <img src="images/Conteudo/fita.png" alt="fita de algodão" />
+            <h2>Produtos</h2>
+            <button>Ver Mais</button>
+          </div>
+          <div className="contact-bottom-square-2">
+            <img
+              src="images/Conteudo/contacto.png"
+              alt="portátil e telemovel"
+            />
+            <h2>Contactos</h2>
+            <button>Ver Mais</button>
+          </div>
+        </section>
       </main>
     </>
   );
